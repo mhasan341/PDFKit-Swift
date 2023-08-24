c8b07b8db7b8eef1044ea71d98a08fe8a2eb5cad
Mahmudul Hasan <mhasan341@gmail.com>
2023-08-25 00:11:23 +0600

    We rendered pdf!

--- PDFKit-Swift/ViewController.swift
+++ PDFKit-Swift/ViewController.swift
@@ -10,11 +10,35 @@ import PDFKit
 
 class ViewController: UIViewController {
 
+
+    let pdfView = PDFView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        view.addSubview(pdfView)
+        pdfView.frame = view.bounds
+
+        loadPDF()
     }
 
+    // in an actual scenario we could fetch from device/network whatever
+    func loadPDF(){
+        guard let path = Bundle.main.path(forResource: "MM_Latest", ofType: "pdf") else {return}
 
+        let url:URL
+
+        if #available(iOS 16.0, *) {
+            url = URL(filePath: path)
+        } else {
+            // Fallback on earlier versions
+            url = URL(fileURLWithPath: path)
+        }
+
+        guard let pdfDocument = PDFDocument(url: url) else {return}
+        pdfView.document = pdfDocument
+        pdfView.autoScales = true
+    }
 }
 
